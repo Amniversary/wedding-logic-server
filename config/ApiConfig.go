b46eabaeba95f33a7aa0cd943d824bd97059af24
Gavean <623006297@qq.com>
2018-02-01 17:27:03 +0800

    update

--- config/ApiConfig.go
+++ config/ApiConfig.go
@@ -70,19 +70,21 @@ type UserCardList struct {
 }
 
 type UserCardInfo struct {
-	ID           int64  `json:"id"`
-	UserId       int64  `json:"user_id"`
-	Name         string `json:"name"`
-	Pic          string `json:"pic"`
-	Phone        string `json:"phone"`
-	Professional string `json:"professional"`
-	Year         string `json:"year"`
-	Company      string `json:"company"`
-	Site         string `json:"site"`
-	Accessment   string `json:"accessment"`
-	Fame         int64  `json:"fame"`
-	Lick         int64  `json:"lick"`
-	IsLick       int64  `json:"is_lick"`
+	ID           int64   `json:"id"`
+	UserId       int64   `json:"user_id"`
+	Name         string  `json:"name"`
+	Pic          string  `json:"pic"`
+	Phone        string  `json:"phone"`
+	Professional string  `json:"professional"`
+	Year         string  `json:"year"`
+	Company      string  `json:"company"`
+	Site         string  `json:"site"`
+	Accessment   string  `json:"accessment"`
+	Fame         int64   `json:"fame"`
+	Lick         int64   `json:"lick"`
+	Longitude    float64 `json:"longitude"`
+	Latitude     float64 `json:"latitude"`
+	IsLick       int64   `json:"is_lick"`
 }
 
 type NewDynamic struct {
